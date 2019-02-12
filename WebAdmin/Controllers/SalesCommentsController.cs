8b50117b677242843cba6853af1d70e0d83afe39
victordoom503 <victordoom503@outlook.com>
2019-02-12 15:46:43 -0600

    Sales Comments Falta Agregar

--- WebAdmin/Controllers/SalesCommentsController.cs
+++ WebAdmin/Controllers/SalesCommentsController.cs
@@ -32,7 +32,7 @@ namespace WebAdmin.Controllers
                 //return RedirectToAction("Index", "Login");
             }
 
-            var Employees = _context.Employees;
+            var Employees = _context.Employees.Where(x => x.DepartmentId == 2);
 
             string Id_of_AspNetUser = _clasess.ExtensionMethods.getUserId(this.User);
             string Email = this.User.FindFirstValue(ClaimTypes.Name);
@@ -52,126 +52,7 @@ namespace WebAdmin.Controllers
             return View(await Employees.ToListAsync());
         }
 
-        // GET: SalesComments/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var employees = await _context.Employees
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (employees == null)
-            {
-                return NotFound();
-            }
-
-            return View(employees);
-        }
-
-        // GET: SalesComments/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: SalesComments/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Company,LastName,FirstName,EmailAddress,JobTitle,BusinessPhone,HomePhone,MobilePhone,FaxNumber,Address,City,StateProvince,ZippostalCode,CountryRegion,WebPage,Notes,Attachments,Social,TaxId,Birthdate,Usuario,Pass,Admin,Accounting,Administration,UsuariosData,Salesdepartament,Technicalsupport,Inventory,MostrarCintaOpciones,ActivarShift,OfficeCode,CreatedDate,CreatedBy,LastModification,DepartmentId,Status,PositionId")] Employees employees)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(employees);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(employees);
-        }
-
-        // GET: SalesComments/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var employees = await _context.Employees.FindAsync(id);
-            if (employees == null)
-            {
-                return NotFound();
-            }
-            return View(employees);
-        }
-
-        // POST: SalesComments/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Company,LastName,FirstName,EmailAddress,JobTitle,BusinessPhone,HomePhone,MobilePhone,FaxNumber,Address,City,StateProvince,ZippostalCode,CountryRegion,WebPage,Notes,Attachments,Social,TaxId,Birthdate,Usuario,Pass,Admin,Accounting,Administration,UsuariosData,Salesdepartament,Technicalsupport,Inventory,MostrarCintaOpciones,ActivarShift,OfficeCode,CreatedDate,CreatedBy,LastModification,DepartmentId,Status,PositionId")] Employees employees)
-        {
-            if (id != employees.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(employees);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!EmployeesExists(employees.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(employees);
-        }
-
-        // GET: SalesComments/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var employees = await _context.Employees
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (employees == null)
-            {
-                return NotFound();
-            }
-
-            return View(employees);
-        }
-
-        // POST: SalesComments/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var employees = await _context.Employees.FindAsync(id);
-            _context.Employees.Remove(employees);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
+        
         private bool EmployeesExists(int id)
         {
             return _context.Employees.Any(e => e.Id == id);
@@ -186,6 +67,43 @@ namespace WebAdmin.Controllers
             
 
             return  Comments;
+        }
+
+        public async Task<bool> CrearComments(int salesid, int commentby, string title, string comment)
+        {
+            var NewComment = new SalesComments
+            {
+                SalesId = salesid,
+                CommentBy = commentby,
+                Title = title,
+                Comment = comment
+            };
+
+            var result = await _context.AddAsync(NewComment);
+            await _context.SaveChangesAsync();
+
+            return true;
+
+        }
+        
+        public bool getByTo (int by, int to)
+        {
+            bool resp = false;
+
+
+
+            return resp;
+        }
+
+        public async Task<List<SelectListItem>> GetAdmin()
+        {
+
+            
+            List<SelectListItem> admins = new List<SelectListItem>();
+
+           
+
+            return admins;
         }
     }
 }
--- WebAdmin/wwwroot/js/Sales.js
+++ WebAdmin/wwwroot/js/Sales.js
@@ -15,12 +15,27 @@ function getComments(id, action) {
 }
 
 function mostrarComments(response) {
+
+   
+    var row = '';
+    
     items = response;
-    var row;
+    row += '<div class="text-right">';
+    row += '<button class="btn btn-warning" data-toggle="modal" data-target="#modalAgregarComments">Crear</button>';
+    row += '</div>';
+    row += '<table  class="table  table-condensed  table-hover wrap dt-responsive cell-border compact stripe row-border" style="width:100%">'
+    row += '<thead><tr><th>By/Datetime</th><th>Title/Comment</th></tr></thead>'
     $.each(items, function (index, val) {
-        row += "<tr><td>" + val.comment + "</td></tr>";
+        
+        row += '<tr>'
+        row += '<td><b>' + val.commentBy + '</b><br> <p>' + val.commentDatetime + '</p></td>';
+        row += '<td><b>' + val.title + '</b> <br> <p>' + val.comment + '</p></td>';
+        //row += '<td>' + val.comment + '</td>';
+        row += '</tr>'
 
     });
 
-    $("#mitabla").append(row);
+    row += '</table>'
+
+    $('#modalcomment').html(row);
 }
